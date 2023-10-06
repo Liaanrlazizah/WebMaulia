0f0aba8881c1d7d054c8b7f99e4b8e13bf203604
Liaanrlazizah <maulia.nurulazizah99@gmail.com>
2023-10-06 19:45:55 +0700

    WebMaulia

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// toogle & responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+
+    burger.addEventListener("click", () =>{
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toogle-burger");
+    });
+};
+
+navSlide();
+
+// clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
